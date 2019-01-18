7e7dc2739c5552f7ede545fc8ba7d846d520c926
Djeezuss <olivierleb25@gmail.com>
2019-01-17 19:36:36 -0500

    Cleaned up routes.rb and fixed the css of the alert boxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :voitures
   root 'voitures#index'
+
+  get  '/voitures',       to: 'voitures#index', as: 'voitures'
+  post '/voitures',       to: 'voitures#create'
+  get  '/voitures/new',   to: 'voitures#new',   as: 'new_voiture'
+  get  '/voitures/:id',   to: 'voitures#show',  as: 'voiture'
+  patch  '/voitures/:id', to: 'voitures#update'
+  put  '/voitures/:id',   to: 'voitures#update'
+  delete '/voitures/:id', to: 'voitures#destroy'
 end
